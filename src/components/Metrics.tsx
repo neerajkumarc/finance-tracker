3074d13de5bc34bd21d801416eb9097252707161
neerajkumarc <chaudharyneeraj91@gmail.com>
2025-02-09 22:46:45 +0530

    fix(ui): show number fixed to 0

--- src/components/Metrics.tsx
+++ src/components/Metrics.tsx
@@ -7,7 +7,7 @@ const Metrics = ({ balance, expenses, income }: { balance: number, expenses: num
       {/* Balance Card */}
       <Card className="p-4 rounded-xl space-y-1">
         <div className="flex justify-between items-center">
-          <span className="text-3xl font-semibold">₹{balance.toFixed(2)}</span>
+          <span className="text-3xl">₹{balance.toFixed(0)}</span>
         </div>
         <div className="text-muted-foreground flex items-center gap-1">total</div>
       </Card>
@@ -18,7 +18,7 @@ const Metrics = ({ balance, expenses, income }: { balance: number, expenses: num
           <div className="flex items-center gap-2">
             <span className="flex justify-center items-center h-8 w-8 rounded-full bg-red-400/30">↗</span>
           </div>
-          <span className="text-2xl font-semibold">₹{expenses.toFixed(2)}</span>
+          <span className="text-2xl">₹{expenses.toFixed(0)}</span>
           <div className="flex items-center gap-2 text-sm">
             <span className="text-muted-foreground">expenses</span>
           </div>
@@ -28,7 +28,7 @@ const Metrics = ({ balance, expenses, income }: { balance: number, expenses: num
           <div className="flex items-center gap-2">
             <span className="flex justify-center items-center h-8 w-8 rounded-full bg-green-400/30">↙</span>
           </div>
-          <span className="text-2xl font-semibold">₹{income.toFixed(2)}</span>
+          <span className="text-2xl">₹{income.toFixed(0)}</span>
           <div className="flex items-center gap-2 text-sm">
             <span className="text-muted-foreground">income</span>
           </div>
